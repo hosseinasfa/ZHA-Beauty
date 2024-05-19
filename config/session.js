ac5f88e0519f137c7a53cc793fd059206bbdec6a
hosseinasfa <asfa.hossein@gmail.com>
2024-05-19 12:53:28 +0330

    edit

--- config/session.js
+++ config/session.js
@@ -7,5 +7,5 @@ module.exports = {
     resave: true,
     saveUninitialized: true,
     cookie : { expires : new Date(Date.now() + 1000 * 60 * 60 * 5 ) },
-    store: MongoStore.create({ mongoUrl: 'mongodb://root:OLGEOtTVaTQT6EpW9YCgv06J@esme.iran.liara.ir:30472/ZHA-Beauty?authSource=admin&replicaSet=rs0&directConnection=true' })
+    store: MongoStore.create({ mongoUrl: process.env.DATABASE_URL })
 }
